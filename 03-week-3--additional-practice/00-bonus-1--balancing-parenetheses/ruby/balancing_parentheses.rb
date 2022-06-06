cf07b46349d380a5c3b484aeb4650f10d3589cad
Michael Dally <mddally@gmail.com>
2022-06-06 15:12:45 -0400

    Balance paren in ruby

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
@@ -1,5 +1,19 @@
 def balancing_parentheses(string)
   # type your code in here
+  parArr = []
+  string.split('').each { |char|
+    if char == '('
+      parArr.push(char)
+    end
+    if(char == ')')
+      if(parArr[-1] == '(')
+        parArr.pop()
+      else
+        parArr.push(char)
+      end
+    end
+  }
+  parArr.length
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -21,3 +35,11 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+  # create a paranthesis stack
+  # go through string by character
+  # if character is open paren push it to stack
+  # if close paren check to see if top of stack is open paren 
+    # if they are a match open/close pop the open paren off stack
+    # if its not a match push the close paren on to the stack
+  # return the length of the stack
+
