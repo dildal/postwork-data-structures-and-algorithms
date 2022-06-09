8ae84e6c4a94506dd14a2c11bc714abf3c2159c5
Michael Dally <mddally@gmail.com>
2022-06-09 11:07:47 -0400

    Implement queue class

--- 06-week-5--big-o-continued/01-days-3-to-4--implement-a-queue-class/ruby/queue.rb
+++ 06-week-5--big-o-continued/01-days-3-to-4--implement-a-queue-class/ruby/queue.rb
@@ -11,35 +11,47 @@ class Queue
   # add item to rear of queue if not full
   # if full, throw error
   def enqueue(item)
+    if(size == limit)
+      raise "Queue is full"
+    else
+      @queue << item
+    end
   end
 
   # remove item from front of queue and return it
   def dequeue
+    @queue.shift
   end
 
   # return item at front of queue without removing it
   def peek
+    @queue[0]
   end
 
   # return true if queue is empty, otherwise false
   def isEmpty?
+    size.zero?
   end
 
   # return true if queue is full, otherwise false
   def isFull?
+    size == limit
   end
 
   # return number of items in queue
   def size
+    @queue.length
   end
 
   # return -1 if item not in queue, otherwise integer representing 
   # how far it is from the front
   def search(target)
+    queue.index(target) ? queue.index(target) : -1
   end
 
   # print contents of queue: do not return the queue itself!
   def print
+    puts queue.join(" -> ")
   end
 end
 
